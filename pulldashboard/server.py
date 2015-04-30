91f583dac1083d7ce3e548ddda1021718c4af252
Simon Chapman <sdchapman@gmail.com>
2015-04-30 12:06:51 +0100

    PEP8 fixes

--- pulldashboard/server.py
+++ pulldashboard/server.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from flask import render_template
 from pulldashboard import app
 from pulldashboard.models import PullRequest, JenkinsProject
@@ -8,13 +7,17 @@ import time
 import re
 from calendar import timegm
 
+
 @app.route('/')
 def index():
-    url = app.config['GITHUB_API_ISSUES_URL'] + app.config['GITHUB_API_ISSUES_FILTER']
+    """
+    Constructs the index page.
+    """
+    url = app.config['GITHUB_API_ISSUES_URL'] + \
+        app.config['GITHUB_API_ISSUES_FILTER']
 
     pulls = []
     ciprojects = []
-
 
     # Required headers for a GET request
     response = requests.get(url, headers=app.config['GITHUB_API_HEADERS'])
@@ -25,28 +28,32 @@ def index():
             if raw_issue['repository']['full_name'] in excludedRepos:
                 break
 
-            # Check if issue is actually a pull request and add to list if it is
+            # Check if issue is actually a pull request and add to list if it
+            # is
             if 'pull_request' in raw_issue:
-                pr = PullRequest(
+                pull_request = PullRequest(
                     raw_issue['number'],
                     raw_issue['title'],
                     raw_issue['user']['login'],
-                    timegm(time.strptime(raw_issue['created_at'].replace('Z', 'GMT'), '%Y-%m-%dT%H:%M:%S%Z')),
+                    timegm(time.strptime(
+                        raw_issue['created_at'].replace('Z', 'GMT'), '%Y-%m-%dT%H:%M:%S%Z')),
                     raw_issue['repository']['name'],
                     raw_issue['html_url']
-                    )
-                pulls.append(pr)
+                )
+                pulls.append(pull_request)
     try:
-
-        response = requests.get(app.config['JENKINS_URL'], auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASSWORD']))
+        response = requests.get(app.config['JENKINS_URL'], auth=(
+            app.config['JENKINS_USER'], app.config['JENKINS_PASSWORD']))
         if response.status_code == requests.codes.ok:
             jenkins_raw = response.json()
             for jenkins_projects in jenkins_raw['jobs']:
 
-                result = re.findall(r'\(Controller\)', jenkins_projects['name'])
-                if (len(result) == 0):
-                    result = re.findall(r'\(Branch\)', jenkins_projects['name'])
-                    if (len(result) == 0):
+                result = re.findall(
+                    r'\(Controller\)', jenkins_projects['name'])
+                if len(result) == 0:
+                    result = re.findall(
+                        r'\(Branch\)', jenkins_projects['name'])
+                    if len(result) == 0:
 
                         if jenkins_projects['buildable'] == True:
                             try:
@@ -55,11 +62,14 @@ def index():
                                     status = 'Passing'
 
                                 url = jenkins_projects['lastBuild']['url']
-                                timestamp = jenkins_projects['lastBuild']['timestamp']
-                                culprits = jenkins_projects['lastBuild']['culprits']
-                                number = jenkins_projects['lastBuild']['number']
+                                timestamp = jenkins_projects[
+                                    'lastBuild']['timestamp']
+                                culprits = jenkins_projects[
+                                    'lastBuild']['culprits']
+                                number = jenkins_projects[
+                                    'lastBuild']['number']
 
-                            except:
+                            except Exception:
                                 status = 'Notrun'
                                 url = ''
                                 timestamp = ''
@@ -76,23 +86,26 @@ def index():
                             )
                             ciprojects.append(ciproject)
 
-
-
         # Sort by time, oldest first as that's the most important to sort out
         pulls.sort(key=lambda x: x.created_at, reverse=False)
         ciprojects.sort(key=lambda x: x.status, reverse=False)
 
         return render_template("index.html", pulls=pulls, ciprojects=ciprojects)
 
-    except Exception as e:
-        return render_template("jenkins_error.html", jenkins_url=app.config['JENKINS_URL'], error=e)
+    except Exception as exc:
+        return render_template(
+            "jenkins_error.html", jenkins_url=app.config['JENKINS_URL'], error=exc)
 
 
-#  Some useful headers to set to beef up the robustness of the app
-# https://www.owasp.org/index.php/List_of_useful_HTTP_headers
 @app.after_request
 def after_request(response):
-    response.headers.add('Content-Security-Policy', "default-src 'self' ajax.googleapis.com maxcdn.bootstrapcdn.com fonts.gstatic.com fonts.googleapis.com data:")
+    """
+    Some useful headers to set to beef up the robustness of the app
+    https://www.owasp.org/index.php/List_of_useful_HTTP_headers
+    """
+    response.headers.add('Content-Security-Policy',
+                         ("default-src 'self' ajax.googleapis.com maxcdn.bootstrapcdn.com "
+                          "fonts.gstatic.com fonts.googleapis.com data:"))
     response.headers.add('X-Frame-Options', 'deny')
     response.headers.add('X-Content-Type-Options', 'nosniff')
     response.headers.add('X-XSS-Protection', '1; mode=block')
