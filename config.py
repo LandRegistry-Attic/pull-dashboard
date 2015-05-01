8e72cedf93101f22647acaff8a86efd37231aeb2
Simon Chapman <sdchapman@gmail.com>
2015-05-01 14:10:36 +0100

    Change how config works

--- config.py
+++ config.py
@@ -1,28 +1,35 @@
 import os
 
+# Get things from environment
+settings = os.environ.get('SETTINGS')
+github_api_key = os.environ.get('GITHUB_API_KEY')
+jenkins_host = os.environ.get('JENKINS_HOST')
+jenkins_user = os.environ.get('JENKINS_USER')
+jenkins_password = os.environ.get('JENKINS_PASSWORD')
 
-class Config(object):
-    DEBUG = False
-    REPOSITORY_BLACKLIST = 'Blacklistfile'
-    GITHUB_API_ISSUES_URL = "https://api.github.com/orgs/LandRegistry/issues"
+# The config dict to be used in public
+CONFIG_DICT = {
+    'DEBUG': False,
+    'REPOSITORY_BLACKLIST': 'Blacklistfile',
+    'GITHUB_API_ISSUES_URL': "https://api.github.com/orgs/LandRegistry/issues",
     # split parameter out so URL config var can be used for response mocking
-    GITHUB_API_ISSUES_FILTER = "?filter=all"
-    GITHUB_API_HEADERS = {
+    'GITHUB_API_ISSUES_FILTER': "?filter=all",
+    'GITHUB_API_HEADERS': {
         'User-Agent': 'LandRegistry-Build-Dashboard/1.0.0',
         'Accept': 'application/vnd.github.v3+json',
         'Content-type': 'application/json',
-        'Authorization': 'token %s' % os.environ.get('GITHUB_API_KEY')
-    }
-    JENKINS_URL = os.environ.get(
-        'JENKINS_HOST') + '/api/json?pretty=true&depth=2&&tree=jobs[name,buildable,lastBuild[number,duration,timestamp,culprits[fullName],result,url,changeSet[items[msg,author[fullName]]]]]'
-    JENKINS_USER = os.environ.get('JENKINS_USER')
-    JENKINS_PASSWORD = os.environ.get('JENKINS_PASSWORD')
+        'Authorization': 'token {}'.format(github_api_key)
+    },
+    'JENKINS_URL': jenkins_host + '/api/json?pretty=true&depth=2&&tree=jobs[name,buildable,lastBuild[number,duration,timestamp,culprits[fullName],result,url,changeSet[items[msg,author[fullName]]]]]',
+    'JENKINS_USER': jenkins_user,
+    'JENKINS_PASSWORD': jenkins_password
+}
 
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestConfig(DevelopmentConfig):
-    DEBUG = True
-    TESTING = True
+# Make further tweaks based on environment
+if settings == 'dev':
+    CONFIG_DICT['DEBUG'] = True
+elif settings == 'test':
+    CONFIG_DICT['LOGGING'] = False
+    CONFIG_DICT['DEBUG'] = True
+    CONFIG_DICT['TESTING'] = True
---
+++ environment_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export SETTINGS='test'
+export GITHUB_API_KEY='xxx'
+export JENKINS_HOST='http://127.0.0.1'
+export JENKINS_USER='user'
+export JENKINS_PASSWORD='password'
